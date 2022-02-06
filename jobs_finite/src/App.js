bab381d2d7fd4bce134adcb635d70d68b65f5b67
kartheek kumar <kartheek12345kumar@gmail.com>
2022-02-06 13:26:31 +0530

    initial commit to main branch

---
+++ jobs_finite/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>JOBS FINITE</h1>
+     <h2>Hey Guys Welcome to jobs finite</h2>
+    </div>
+  );
+}
+
+export default App;
